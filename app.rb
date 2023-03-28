477e9d895a4b55ff0ac1c7e158cafe61838d547c
jingleville <andrewgavrick@yandex.ru>
2023-03-28 20:47:31 +0300

    prepared all files(2)

---
+++ app.rb
@@ -0,0 +1,21 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+
+
+
+
+# '/'
+# posts
+
+# '/new'
+# create new post
+
+# '/authors'
+# authors, click on author return /author with all posts by author
